2f466db942dd0957ab4dc24d86ca3df41699afe3
zunia25 <82039843+zunia25@users.noreply.github.com>
2023-10-02 22:23:15 +0700

    Add files via upload

---
+++ Tugas 2/js/script.js
@@ -0,0 +1,23 @@
+const textElement = ['Freelancer','Student at ITS'];
+let count = 0;
+let txtIndex = 0;
+let currentTxt = '';
+let words = '';
+
+(function ngetik(){
+    if(count == textElement.length){
+        count = 0;
+    }
+
+    currentTxt = textElement[count];
+
+    words = currentTxt.slice(0, ++txtIndex);
+    document.querySelector('.efek-ngetik').textContent = words;
+
+    if(words.length == currentTxt.length){
+        count++;
+        txtIndex = 0;
+    }
+
+    setTimeout(ngetik, 500);
+})();
